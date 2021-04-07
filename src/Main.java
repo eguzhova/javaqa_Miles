4e0cf32a744a597839c2e0ce5ae8f09411d8de29
eguzhova <81353538+eguzhova@users.noreply.github.com>
2021-04-07 16:51:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        long ticketPrice = 18495_25;
+        long milePrice = 20;
+        long mileTotal = ticketPrice/ 100 / milePrice ;
+        System.out.println("Количество миль начисленных за билет:"+mileTotal);
+    }
+}
+
+
+
